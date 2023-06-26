2053f2ddba685724bcbb036949519ec4123ecb3c
TambePranav <136703398+TambePranav@users.noreply.github.com>
2023-06-26 18:53:39 +0530

    Time: 84 ms (30.60%), Space: 48.6 MB (26.42%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    
+public:
+    bool canJump(vector<int>& nums) 
+    {
+       int minjumps=0;
+       for(int i=nums.size()-2;i>=0;i--)
+       {
+           minjumps++;
+           if(nums[i]>=minjumps)
+           {
+               minjumps=0;
+           }
+       }
+       if(minjumps==0)return true;
+       return false;
+        
+    }
+};
