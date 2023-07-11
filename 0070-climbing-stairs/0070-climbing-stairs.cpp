a8993e5f6b6872bdf2e9d307794cdf787055055a
TambePranav <136703398+TambePranav@users.noreply.github.com>
2023-07-11 09:19:25 +0530

    Time: 3 ms (35.06%), Space: 5.9 MB (95.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,8 +2,9 @@ vector<int>ans(46,0);
 class Solution {
 public:
     int climbStairs(int n) {
+        ans[0]=1;
         ans[1]=1;ans[2]=2;
-        for(int i=3;i<ans.size();i++)
+        for(int i=2;i<ans.size();i++)
         {
             ans[i]=ans[i-1]+ans[i-2];
         }
